842c6f6a89716d68174866a3b7a54020df662a0e
Noah <noahpfst@gmail.com>
2026-01-07 12:29:21 +0100

    pwa dynamic theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -14,11 +13,34 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // sync pwa theme color with rest of the site before load and on theme change
+  const themeScript = `
+    (function() {
+      function updateThemeColor() {
+        // from globals.css
+        const darkThemeColor = '#0a0a0a';
+        const lightThemeColor = '#ffffff';
+        
+        const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+        
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          metaThemeColor.setAttribute('content', darkThemeColor);
+        } else {
+          metaThemeColor.setAttribute('content', lightThemeColor);
+        }
+      }
+      // set initial
+      updateThemeColor();
+      // theme change listener
+      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateThemeColor);
+    })();
+  `;
   return (
     <html lang="en" className="scroll-smooth">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#dddddd" />
+        <script dangerouslySetInnerHTML={{ __html: themeScript }} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="NP" />
